fafb0a3643a4478e562cd1c415306eeadcb2d69a
Getse <getsee.to@gmail.com>
2020-01-21 17:48:06 -0600

    Cambio en el nombre del botón
    Indentación y acomodo de componentes
    
    Se borraron componentes de App.js para redireccionar después de hacer LogIn y LogOut

--- src/components/subbanner.jsx
+++ src/components/subbanner.jsx
@@ -1,33 +1,33 @@
 import React from "react";
-import { Grid, Segment,Icon,Header, Input } from "semantic-ui-react";
+import { Grid, Segment, Icon, Header, Input } from "semantic-ui-react";
 
 
 const Subbanner = () => {
   return (
-    <Segment color="grey" inverted>
+    <Segment color="grey" inverted size="mini">
       <Grid columns={3} divided>
-      <Grid.Row>
-      <Grid.Column>
+        <Grid.Row>
+          <Grid.Column>
 
-    <Icon.Group as="h1" size='large'>
-      <Icon name='home' /> Inicio
+            <Icon.Group as="h3" size='small'>
+              <Icon name='home' /> Inicio
     </Icon.Group>
-    
-      </Grid.Column>
-      <Grid.Column>
-      <Icon.Group as="h1" size='large'>
-      <Icon name="search" /> Buscar 
-      <Input size="mini" placeholder='Buscar...' />
+
+          </Grid.Column>
+          <Grid.Column>
+            <Icon.Group as="h3" size='large'>
+              <Icon name="search" /> Buscar <Input size="mini" placeholder='Busca aquí' />
+            </Icon.Group>
+
+
+          </Grid.Column>
+          <Grid.Column>
+            <Icon.Group as="h3" size='large'>
+              <Icon name="user" /> Usuario
     </Icon.Group>
-    
-      </Grid.Column>
-      <Grid.Column>
-      <Icon.Group as="h1" size='large'>
-      <Icon name="user" /> Usuario
-    </Icon.Group>
-      </Grid.Column>
-    </Grid.Row> 
-    </Grid>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Segment>
   );
 };
