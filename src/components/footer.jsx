00467722850a7d879258f0f0339be2ea86b803a4
PacoZapien <53961739+PacoZapien@users.noreply.github.com>
2020-02-12 12:26:25 -0600

    Add files via upload
    footer v2

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import { Grid, Segment } from "semantic-ui-react";
+import { Grid, Segment, Image } from "semantic-ui-react";
+import coloreszap from '../img/coloreszap.jpg';
 
 const Footer = () => {
     return (
         <Segment>
             <Grid divided="vertically">
                 <Grid.Row columns={3}>
-                    <Grid.Column>
-                        <h3> Este es un footer</h3>
+                    <Grid.Column textAlign='center'>
+                        <font color="#1e636d"> <p> 2020- ZapAcademy</p> </font>
                     </Grid.Column>
                     <Grid.Column>
-                        {/* <Image  fluid centered    width="100" heigth="30" src="https://servicios.zapopan.gob.mx:8000/wwwportal/publicfiles/inline-images/iniciozapopan2019-1915x215_1.png" 
-              /> */}
+                        
+                        <Image src={coloreszap}  class="ui medium centered image"/> 
 
                     </Grid.Column>
 
-                    <Grid.Column>
-                        <h3>
-                            Municipio de Zapopan, Jalisco
-                            Avenida Hidalgo 151, Zapopan, 45100 Zapopan, Jal.
-                            33 3818 2200 </h3>
+                    <Grid.Column textAlign='right'>
+                        <font color="#1e636d">
+                            
+                            <p>
+                                Municipio de Zapopan, Jalisco
+                                Avenida Hidalgo 151, Zapopan, 45100 Zapopan, Jal.
+                            33 3818 2200 </p>
+                        </font>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
