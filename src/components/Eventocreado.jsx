1b505763511d168b8b7894fff43ed201c45d6347
Vciol <57044775+Vciol@users.noreply.github.com>
2020-02-13 13:43:51 -0600

    Evento creado
    página de evento creado y botón de regresar

---
+++ src/components/Eventocreado.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Header, Container, Button } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+
+const Eventocreado = () => (
+    <Container>
+        <Header as='h1' textAlign='center'>
+            ¡Su evento ha sido creado!
+            <div>
+                <Button negative>Regresar</Button>
+            </div>
+        </Header>
+
+    </Container>
+)
+
+export default Eventocreado
